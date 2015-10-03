86816a14aa437f7494514134a492950c202947c5
fernandolicon <wisimata@gmail.com>
2015-10-03 10:15:13 -0600

    Updated README & added example project

---
+++ Example/DateDifferenceExample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DateDifferenceExample
+//
+//  Created by Luis Fernando Mata on 3/10/15.
+//  Copyright © 2015 Luis Fernando Mata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # LFMDateDifferenceToString
+
+## Description
+
+This is a simple library that receives a date difference and returns it as a string.
+
+For example, if you send a Date Difference of **300s**, the library will return **5m**.
+
+Other examples:
+
+* 7,200s - 2h
+* 88,000 - Yesterday
+* 172,800 - 2 days ago
+
+## Methods
+
+Being developed.
